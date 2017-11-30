ec4c510bbf062bb7d862e32a75e26d1aa3c5785e
GOPINATHSWAMINATHAN <gopinathswaminadane@gmail.com>
2017-11-30 12:10:15 +0300

    Get Current Location of driver when switch on

--- app/src/main/java/com/hlacab/hladriver/Profile.java
+++ app/src/main/java/com/hlacab/hladriver/Profile.java
@@ -1,13 +1,31 @@
 package com.hlacab.hladriver;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Profile extends AppCompatActivity {
-
+EditText username,password;
+Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        username=(EditText)findViewById(R.id.username);
+        password=(EditText)findViewById(R.id.password);
+        String user=username.getText().toString();
+        String pass=password.getText().toString();
+
+        Intent i=new Intent(this,MapsActivity.class);
+        Bundle b=new Bundle();
+        b.putString("user",user);
+        b.putString("pass",pass);
+        i.putExtras(b);
+        startActivity(i);
+
+
     }
 }
